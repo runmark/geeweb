78c3a2826bbf6f6d7ead248947e25952ee2df3d0
Ryan Mark <ma_xingmin@163.com>
2021-08-19 11:58:25 +0800

    feat(gee): extract router from gee

--- gee/gee.go
+++ gee/gee.go
@@ -5,45 +5,30 @@ import (
 )
 
 //type HandlerFunc func(w http.ResponseWriter, r *http.Request)
-
 type HandlerFunc func(*Context)
-
 
 // 类比 SpringMVC，Engine 就是 dispatchservlet
 type Engine struct {
-	router map[string]HandlerFunc
+	router *router
 }
 
 func New() *Engine {
 	return &Engine{
-		router: make(map[string]HandlerFunc),
+		router: newRouter(),
 	}
-}
-
-func (e *Engine) addRoute(method, path string, handlerFunc HandlerFunc) {
-	key := method + "-" + path
-	e.router[key] = handlerFunc
 }
 
 func (e *Engine) GET(path string, handlerFunc HandlerFunc) {
-	e.addRoute("GET", path, handlerFunc)
+	e.router.addRoute("GET", path, handlerFunc)
 }
 
 func (e *Engine) POST(path string, handlerFunc HandlerFunc) {
-	e.addRoute("POST", path, handlerFunc)
+	e.router.addRoute("POST", path, handlerFunc)
 }
 
 func (e *Engine) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	key := r.Method + "-" + r.URL.Path
-
 	c := NewContext(w, r)
-	f, ok := e.router[key]
-	if ok {
-		f(c)
-		return
-	}
-
-	w.WriteHeader(http.StatusNotFound)
+	e.router.handle(c)
 }
 
 func (e *Engine) Run(addr string) error {
